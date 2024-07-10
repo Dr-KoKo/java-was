396038d747f6aa9c8faa840a631d89df6d76121b
donghar <donghar@naver.com>
2024-07-10 10:56:26 +0900

    [Feat] 로그인 기능
    인증, 세션 유지 기능 구현

--- src/main/java/codesquad/application/handler/RequestHandler.java
+++ src/main/java/codesquad/application/handler/RequestHandler.java
@@ -13,29 +13,63 @@ import server.http.model.startline.StatusLine;
 import server.http.model.startline.Version;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class RequestHandler {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final BodyParser parser;
+    private final UserDao userDao;
+    private final SessionStorage sessionStorage;
 
-    public RequestHandler(BodyParser parser) {
+    public RequestHandler(BodyParser parser, UserDao userDao, SessionStorage sessionStorage) {
         this.parser = parser;
+        this.userDao = userDao;
+        this.sessionStorage = sessionStorage;
     }
 
     @PostMapping(path = "/create")
     public HttpResponse create(HttpRequest request) {
-        Map<String, String> parameters = parser.parse(new String(request.getBody().getMessage()));
-        for (Map.Entry<String, String> stringStringEntry : parameters.entrySet()) {
-            logger.debug("Parameter name: {} value: {}", stringStringEntry.getKey(), stringStringEntry.getValue());
-        }
+        Map<String, String> parameters = getBody(request);
         // store user
         User user = new User(parameters.get("userId"), parameters.get("password"), parameters.get("nickname"));
         logger.debug("Creating request processor for user {}", user);
+
+        userDao.save(user);
 
         StatusLine statusLine = new StatusLine(Version.HTTP_1_1, StatusCode.FOUND);
         Headers headers = new Headers();
         headers.addHeader("Location", "/index.html");
         return new HttpResponse(statusLine, headers, null);
+    }
+
+    @PostMapping(path = "/login")
+    public HttpResponse login(HttpRequest request) {
+        Map<String, String> parameters = getBody(request);
+        String userId = parameters.get("userId");
+        String password = parameters.get("password");
+        logger.debug("Login request for user {} with password {}", userId, password);
+
+        Optional<User> findUser = userDao.findByUserId(userId);
+        boolean matches = findUser.isPresent() && findUser.get().matchesPassword(password);
+
+        StatusLine statusLine = new StatusLine(Version.HTTP_1_1, StatusCode.FOUND);
+        Headers headers = new Headers();
+        if (matches) {
+            String sid = sessionStorage.store(findUser.get());
+            headers.addHeader("Location", "/index.html");
+            headers.addHeader("Set-Cookie", "sid=" + sid + "; Path=/");
+        } else {
+            headers.addHeader("Location", "/user/login_failed.html");
+        }
+        return new HttpResponse(statusLine, headers, null);
+    }
+
+    private Map<String, String> getBody(HttpRequest request) {
+        Map<String, String> parameters = parser.parse(new String(request.getBody().getMessage()));
+        for (Map.Entry<String, String> stringStringEntry : parameters.entrySet()) {
+            logger.debug("Parameter name: {} value: {}", stringStringEntry.getKey(), stringStringEntry.getValue());
+        }
+        return parameters;
     }
 }

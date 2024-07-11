f715f56a53745f011c66377f449d899f0dab1d10
donghar <donghar@naver.com>
2024-07-11 18:16:56 +0900

    [Fix] 정적/동적 리소스 분리에 따른 Handler 수정

--- src/main/java/codesquad/application/handler/RequestHandler.java
+++ src/main/java/codesquad/application/handler/RequestHandler.java
@@ -1,5 +1,6 @@
 package codesquad.application.handler;
 
+import codesquad.annotation.GetMapping;
 import codesquad.annotation.PostMapping;
 import codesquad.application.model.User;
 import codesquad.application.parser.BodyParser;
@@ -7,12 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import server.http.model.HttpRequest;
 import server.http.model.HttpResponse;
+import server.http.model.body.Body;
+import server.http.model.header.ContentType;
 import server.http.model.header.Header;
 import server.http.model.header.Headers;
 import server.http.model.startline.StatusCode;
 import server.http.model.startline.StatusLine;
 import server.http.model.startline.Version;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
 import java.util.Optional;
 
@@ -29,7 +35,25 @@ public class RequestHandler {
         this.sessionStorage = sessionStorage;
     }
 
-    @PostMapping(path = "/create")
+    @GetMapping(path = "/")
+    public HttpResponse index(HttpRequest request) {
+        URL resourceUrl = getResourceUrl("/templates/index.html");
+        return responseResource(resourceUrl);
+    }
+
+    @GetMapping(path = "/user/registration")
+    public HttpResponse createUser(HttpRequest request) {
+        URL resourceUrl = getResourceUrl("/templates/user/registration/index.html");
+        return responseResource(resourceUrl);
+    }
+
+    @GetMapping(path = "/user/login")
+    public HttpResponse loginForm(HttpRequest request) {
+        URL resourceUrl = getResourceUrl("/templates/user/login/index.html");
+        return responseResource(resourceUrl);
+    }
+
+    @PostMapping(path = "/user/create")
     public HttpResponse create(HttpRequest request) {
         Map<String, String> parameters = getBody(request);
         // store user
@@ -40,11 +64,11 @@ public class RequestHandler {
 
         StatusLine statusLine = new StatusLine(Version.HTTP_1_1, StatusCode.FOUND);
         Headers headers = new Headers();
-        headers.addHeader("Location", "/index.html");
+        headers.addHeader("Location", "/");
         return new HttpResponse(statusLine, headers, null);
     }
 
-    @PostMapping(path = "/login")
+    @PostMapping(path = "/user/login")
     public HttpResponse login(HttpRequest request) {
         Map<String, String> parameters = getBody(request);
         String userId = parameters.get("userId");
@@ -58,7 +82,7 @@ public class RequestHandler {
         Headers headers = new Headers();
         if (matches) {
             String sid = sessionStorage.store(findUser.get());
-            headers.addHeader("Location", "/index.html");
+            headers.addHeader("Location", "/");
             headers.addHeader("Set-Cookie", "sid=" + sid + "; Path=/");
         } else {
             headers.addHeader("Location", "/user/login_failed.html");
@@ -69,15 +93,55 @@ public class RequestHandler {
     @PostMapping(path = "/logout")
     public HttpResponse logout(HttpRequest request) {
         String sessionId = getSessionId(request);
-        if(sessionId != null) {
+        if (sessionId != null) {
             sessionStorage.invalidate(sessionId);
         }
 
         StatusLine statusLine = new StatusLine(Version.HTTP_1_1, StatusCode.FOUND);
         Headers headers = new Headers();
         headers.addHeader("Set-Cookie", "sid=" + " " + "; Path=/" + "; Max-age=0");
-        headers.addHeader("Location", "/index.html");
+        headers.addHeader("Location", "/");
         return new HttpResponse(statusLine, headers, null);
+    }
+
+    private HttpResponse responseResource(URL resourceUrl) {
+        if (resourceUrl == null) {
+            return notFoundResponse();
+        }
+        byte[] content;
+        try {
+            content = getContent(resourceUrl);
+        } catch (IOException e) {
+            return serverErrorResponse();
+        }
+        String contentType = getContentType(resourceUrl.getFile());
+        return okResponse(content, contentType);
+    }
+
+    private static byte[] getContent(URL resourceUrl) throws IOException {
+        try (InputStream resourceStream = resourceUrl.openStream()) {
+            return resourceStream.readAllBytes();
+        } catch (IOException e) {
+            logger.error("Failed to read resource: {}", "/index.html", e);
+            throw e;
+        }
+    }
+
+    private URL getResourceUrl(String requestPath) {
+        URL resourceUrl = getClass().getResource(requestPath);
+        if (resourceUrl == null) {
+            logger.info("Resource not found in classpath: {}", requestPath);
+        } else {
+            logger.info("Resource found: {}", resourceUrl);
+        }
+        return resourceUrl;
+    }
+
+    private String getContentType(String filePath) {
+        logger.debug("searching extension for file: {}", filePath);
+        String extension = filePath.substring(filePath.lastIndexOf('.') + 1).toLowerCase();
+        logger.debug("extension: {}", extension);
+        return ContentType.ofExtension(extension).getContentType();
     }
 
     private String getSessionId(HttpRequest request) {
@@ -96,5 +160,19 @@ public class RequestHandler {
             logger.debug("Parameter name: {} value: {}", stringStringEntry.getKey(), stringStringEntry.getValue());
         }
         return parameters;
+    }
+
+    private HttpResponse okResponse(byte[] content, String contentType) {
+        Headers headers = new Headers();
+        headers.addHeader("Content-Type", contentType);
+        return new HttpResponse(new StatusLine(Version.HTTP_1_1, StatusCode.OK), headers, new Body(content));
+    }
+
+    private HttpResponse notFoundResponse() {
+        return new HttpResponse(new StatusLine(Version.HTTP_1_1, StatusCode.NOT_FOUND));
+    }
+
+    private HttpResponse serverErrorResponse() {
+        return new HttpResponse(new StatusLine(Version.HTTP_1_1, StatusCode.INTERNAL_SERVER_ERROR));
     }
 }
